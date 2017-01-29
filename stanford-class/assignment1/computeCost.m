c48f46b4410188e0d1b445d68e8aded4bab51e4f
Ivan Zubenko <vanya.zubenko@gmail.com>
2017-01-29 19:05:16 +0200

    assignment1

---
+++ stanford-class/assignment1/computeCost.m
@@ -0,0 +1,12 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+errors = (X * theta - y);
+J = (errors' * errors) / (2 * m);
+
+end
